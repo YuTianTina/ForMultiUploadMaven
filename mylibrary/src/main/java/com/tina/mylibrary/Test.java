16bb4d43d2588a48a5986ace93cc2919be751219
yutt <yutt@xinguangnet.com>
2017-11-03 15:59:34 +0800

    something was wrong..fix it later

---
+++ mylibrary/src/main/java/com/tina/mylibrary/Test.java
@@ -0,0 +1,15 @@
+package com.tina.mylibrary;
+
+/**
+ * 2017/11/3
+ * e-mail: yutt@xinguangnet.com
+ * Description:
+ *
+ * @author yutt
+ */
+
+public class Test {
+    public void show(){
+
+    }
+}
