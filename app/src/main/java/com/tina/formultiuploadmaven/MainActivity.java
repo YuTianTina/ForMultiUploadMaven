17da1768c28c8c699c21d9b107b4fdd0e8ffe3d6
YuTianTina <yutiantina@gmail.com>
2019-04-02 17:51:53 +0800

    支持指定variant上传maven

--- app/src/main/java/com/tina/formultiuploadmaven/MainActivity.java
+++ app/src/main/java/com/tina/formultiuploadmaven/MainActivity.java
@@ -3,11 +3,17 @@ package com.tina.formultiuploadmaven;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tina.mylibrary.Full;
+import com.tina.mylibrary.Test;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Full.show();
+
     }
 }
---
+++ mylibrary/src/full/java/com/tina/mylibrary/Full.java
@@ -0,0 +1,11 @@
+package com.tina.mylibrary;
+
+/**
+ * @author yutiantian email: yutiantina@gmail.com
+ * @since 2019/4/2
+ */
+public class Full {
+    public static void show(){
+
+    }
+}
